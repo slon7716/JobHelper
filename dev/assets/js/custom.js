56ec665ff7fde166ab60e70f869c4fff5b0af6a3
slon7716 <160377@i.ua>
2025-08-29 21:55:12 +0300

    enter-the-site1 and main-page1

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,11 @@
-  
+              // Привласнюємо клас 'active' поточній сторінці
+let currentPage = window.location.pathname.split("/").pop(); // Отримуємо поточний URL
+// Знаходимо всі посилання у навігації
+const navLinks = document.querySelectorAll('.pages a');
+// Перевіряємо кожне посилання
+navLinks.forEach(link => {
+   const linkHref = link.getAttribute('href');
+   if (linkHref === currentPage) {
+      link.classList.add('active'); // Додаємо клас активному посиланню
+   }
+});
