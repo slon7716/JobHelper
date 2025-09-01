508bf44e83c721d04352b289d9a3606c73b6c393
slon7716 <160377@i.ua>
2025-09-01 23:09:15 +0300

    main-page-2

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -9,3 +9,21 @@ navLinks.forEach(link => {
       link.classList.add('active'); // Додаємо клас активному посиланню
    }
 });
+
+                     // Swiper
+const carouselWeekly = new Swiper('.swiper', {
+   speed: 700,
+   slidesPerView: 'auto',
+   spaceBetween: 16,
+   // centeredSlides: true,
+   freeMode: true,
+   navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+   },
+   // breakpoints: {
+   //   1025: {
+   //     spaceBetween: 40,
+   //   },
+   // },
+ });
